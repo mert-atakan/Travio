0148a17084de07576814ce996d0764491d936f15
mert-atakan <mertatakan9707@gmail.com>
2023-09-12 16:44:44 +0300

    Component isimleri düzenlendi.

--- Travio/TabbarVC/MenuPage/EditProfile/EditProfileVC.swift
+++ Travio/TabbarVC/MenuPage/EditProfile/EditProfileVC.swift
@@ -18,88 +18,89 @@ class EditProfileVC: UIViewController {
         return activity
     }()
     
-    private lazy var view1: UIView = {
-        let v = UIView()
-        v.backgroundColor = Color.systemWhite.chooseColor
-        return v
+    private lazy var dissmissButton: UIButton = {
+        let button = UIButton()
+        button.contentMode = .scaleAspectFit
+        button.setImage(UIImage(named: "dismissButton"), for: .normal)
+        button.addTarget(self, action: #selector(dissmissButtonTapped), for: .touchUpInside)
+        return button
     }()
     
-    private lazy var backButton: UIImageView = {
-        let iv = UIImageView()
-        iv.contentMode = .scaleAspectFit
-        iv.image = UIImage(named: "vector")
-        return iv
+    private lazy var headerLabel: UILabel = {
+        let label = UILabel()
+        label.font = Font.semibold32.chooseFont
+        label.textColor = Color.white.chooseColor
+        label.text = "Edit Profile"
+        return label
     }()
     
-    private lazy var titleLbl: UILabel = {
-        let l = UILabel()
-        l.font = Font.bold32.chooseFont
-        l.textColor = Color.white.chooseColor
-        l.text = "Edit Profile"
-        return l
+    private lazy var containerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = Color.systemWhite.chooseColor
+        return view
     }()
     
-    private lazy var imageview: UIImageView = {
-        let iv = UIImageView()
-        iv.layer.cornerRadius = iv.frame.size.width / 2
-        iv.layer.masksToBounds = true
-        iv.contentMode = .scaleAspectFill
-        return iv
+    private lazy var profileImage: UIImageView = {
+        let profileImage = UIImageView()
+        profileImage.layer.cornerRadius = profileImage.frame.size.width / 2
+        profileImage.layer.masksToBounds = true
+        profileImage.contentMode = .scaleAspectFill
+        return profileImage
     }()
     
-    private lazy var changePhotoBtn: UIButton = {
-        let btn = UIButton()
-        btn.setTitle("Change Photo", for: .normal)
-        btn.setTitleColor(Color.systemBlue.chooseColor, for: .normal)
-        btn.addTarget(self, action: #selector(changePhotoTapped), for: .touchUpInside)
-        return btn
+    private lazy var changePhotoButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Change Photo", for: .normal)
+        button.setTitleColor(Color.systemBlue.chooseColor, for: .normal)
+        button.addTarget(self, action: #selector(changePhotoButtonTapped), for: .touchUpInside)
+        return button
     }()
     
-    private lazy var nameLbl: UILabel = {
-        let l = UILabel()
-        l.font = Font.bold24.chooseFont
-        l.textColor = Color.systemblack.chooseColor
-        l.textAlignment = .center
-        return l
+    private lazy var fullNameLabel: UILabel = {
+        let label = UILabel()
+        label.font = Font.semibold24.chooseFont
+        label.textColor = Color.systemblack.chooseColor
+        label.textAlignment = .center
+        return label
     }()
     
     private lazy var dateView: InfoCustomView = {
-        let v = InfoCustomView()
-        v.layer.cornerRadius = 16
-        v.backgroundColor = Color.white.chooseColor
-        v.imageview.image = UIImage(named: "dateIcon")
-        return v
+        let view = InfoCustomView()
+        view.layer.cornerRadius = 16
+        view.backgroundColor = Color.white.chooseColor
+        view.imageview.image = UIImage(named: "dateIcon")
+        return view
     }()
     
-    private lazy var positionView: InfoCustomView = {
-        let v = InfoCustomView()
-        v.layer.cornerRadius = 16
-        v.backgroundColor = Color.white.chooseColor
-        v.imageview.image = UIImage(named: "positionIcon")
-        v.Lbl.text = "Admin"
-        return v
+    private lazy var roleView: InfoCustomView = {
+        let view = InfoCustomView()
+        view.layer.cornerRadius = 16
+        view.backgroundColor = Color.white.chooseColor
+        view.imageview.image = UIImage(named: "positionIcon")
+        view.Lbl.text = "Admin"
+        return view
     }()
     
-    private lazy var nameView: CustomView = {
-        let v = CustomView()
-        v.titleLabel.text = "Full Name"
-        v.textField.attributedPlaceholder = NSAttributedString(string: "bilge_adam", attributes: v.attributes)
-        return v
+    private lazy var fullNameView: CustomView = {
+        let view = CustomView()
+        view.titleLabel.text = "Full Name"
+        view.textField.attributedPlaceholder = NSAttributedString(string: "bilge_adam", attributes: view.attributes)
+        return view
     }()
     
     private lazy var emailView: CustomView = {
-        let v = CustomView()
-        v.titleLabel.text = "Email"
-        v.textField.attributedPlaceholder = NSAttributedString(string: "bilge_adam", attributes: v.attributes)
-        return v
+        let view = CustomView()
+        view.titleLabel.text = "Email"
+        view.textField.attributedPlaceholder = NSAttributedString(string: "bilge_adam", attributes: view.attributes)
+        return view
     }()
     
-    private lazy var saveBtn: CustomButton = {
-        let btn = CustomButton()
-        btn.layer.cornerRadius = 3
-        btn.setTitle("Save", for: .normal)
-        btn.addTarget(self, action: #selector(saveTapped), for: .touchUpInside)
-        return btn
+    private lazy var saveButton: CustomButton = {
+        let button = CustomButton()
+        button.layer.cornerRadius = 16
+        button.setTitle("Save", for: .normal)
+        button.addTarget(self, action: #selector(saveButtonTapped), for: .touchUpInside)
+        return button
     }()
 
     override func viewDidLoad() {
@@ -110,12 +111,16 @@ class EditProfileVC: UIViewController {
     }
     
     override func viewDidLayoutSubviews() {
-        view1.roundCorners(corners: [.topLeft], radius: 80)
-        imageview.layer.cornerRadius = imageview.frame.size.width / 2
-        imageview.layer.masksToBounds = true
+        containerView.roundCorners(corners: [.topLeft], radius: 80)
+        profileImage.layer.cornerRadius = profileImage.frame.size.width / 2
+        profileImage.layer.masksToBounds = true
     }
     
-    @objc func changePhotoTapped() {
+    @objc func dissmissButtonTapped() {
+        self.dismiss(animated: true)
+    }
+    
+    @objc func changePhotoButtonTapped() {
         let vc = UIImagePickerController()
         vc.sourceType = .photoLibrary
         vc.delegate = self
@@ -123,8 +128,8 @@ class EditProfileVC: UIViewController {
         present(vc, animated: true)
     }
     
-    @objc func saveTapped() {
-        guard let name = nameView.textField.text, let email = emailView.textField.text else {return}
+    @objc func saveButtonTapped() {
+        guard let name = fullNameView.textField.text, let email = emailView.textField.text else {return}
         let imageUrl = viewModal.getImageUrl()
         let body = ["full_name":name, "email": email, "pp_url": imageUrl]
         viewModal.editProfile(body: body)
@@ -144,16 +149,16 @@ class EditProfileVC: UIViewController {
         }
         viewModal.getUserInfo()
         viewModal.configureUserInfo = {user in
-            self.nameLbl.text = user.full_name
-            self.positionView.Lbl.text = user.role
+            self.fullNameLabel.text = user.full_name
+            self.roleView.Lbl.text = user.role
            self.dateView.Lbl.text = self.dateFormat(date: user.created_at)
-            self.nameView.textField.text = user.full_name
+            self.fullNameView.textField.text = user.full_name
             self.emailView.textField.text = user.email
             if user.pp_url != "" {
                 let url = URL(string: user.pp_url)
-                self.imageview.kf.setImage(with: url)
+                self.profileImage.kf.setImage(with: url)
             } else {
-                self.imageview.image = UIImage(systemName: "person.fill")
+                self.profileImage.image = UIImage(systemName: "person.fill")
             }
            
         }
@@ -175,54 +180,54 @@ class EditProfileVC: UIViewController {
     private func setupView() {
         self.navigationController?.isNavigationBarHidden = true
         view.backgroundColor = Color.systemGreen.chooseColor
-        self.view.addSubViews(backButton, titleLbl, view1, imageview, changePhotoBtn, nameLbl, dateView, positionView, nameView, emailView, saveBtn, activity)
+        self.view.addSubViews(dissmissButton, headerLabel, containerView, profileImage, changePhotoButton, fullNameLabel, dateView, roleView, fullNameView, emailView, saveButton, activity)
         setupLayout()
     }
     private func setupLayout() {
-        backButton.edgesToSuperview(excluding: [.bottom,.right], insets: .top(32) + .left(23),usingSafeArea: true)
-        backButton.height(21)
-        backButton.width(24)
+        dissmissButton.edgesToSuperview(excluding: [.bottom,.right], insets: .top(32) + .left(23),usingSafeArea: true)
+        dissmissButton.height(21)
+        dissmissButton.width(24)
         
-        titleLbl.edgesToSuperview(excluding: [.bottom, .left], insets: .top(19) + .right(24),usingSafeArea: true)
-        titleLbl.leftToRight(of: backButton, offset: 24)
-        titleLbl.height(48)
+        headerLabel.edgesToSuperview(excluding: [.bottom, .left], insets: .top(19) + .right(24),usingSafeArea: true)
+        headerLabel.leftToRight(of: dissmissButton, offset: 24)
+        headerLabel.height(48)
         
-        view1.edgesToSuperview(excluding: [.top])
-        view1.topToBottom(of: titleLbl, offset: 54)
+        containerView.edgesToSuperview(excluding: [.top])
+        containerView.topToBottom(of: headerLabel, offset: 54)
         
-        imageview.top(to: view1, offset: 24)
-        imageview.edgesToSuperview(excluding: [.bottom, .top], insets: .left(135) + .right(135))
-        imageview.height(120)
-        imageview.width(120)
+        profileImage.top(to: containerView, offset: 24)
+        profileImage.edgesToSuperview(excluding: [.bottom, .top], insets: .left(135) + .right(135))
+        profileImage.height(120)
+        profileImage.width(120)
         
-        changePhotoBtn.topToBottom(of: imageview, offset: 7)
-        changePhotoBtn.edgesToSuperview(excluding: [.bottom, .top], insets: .left(100) + .right(100))
-        changePhotoBtn.height(18)
+        changePhotoButton.topToBottom(of: profileImage, offset: 7)
+        changePhotoButton.edgesToSuperview(excluding: [.bottom, .top], insets: .left(100) + .right(100))
+        changePhotoButton.height(18)
 
-        nameLbl.topToBottom(of: changePhotoBtn, offset: 7)
-        nameLbl.edgesToSuperview(excluding: [.bottom, .top], insets: .left(50) + .right(50))
-        nameLbl.height(36)
+        fullNameLabel.topToBottom(of: changePhotoButton, offset: 7)
+        fullNameLabel.edgesToSuperview(excluding: [.bottom, .top], insets: .left(50) + .right(50))
+        fullNameLabel.height(36)
 
-        dateView.topToBottom(of: nameLbl, offset: 21)
+        dateView.topToBottom(of: fullNameLabel, offset: 21)
         dateView.leftToSuperview(offset: 24)
         dateView.height(52)
         dateView.width(164)
 
-        positionView.leftToRight(of: dateView, offset: 16)
-        positionView.topToBottom(of: nameLbl, offset: 21)
-        positionView.height(52)
-        positionView.width(164)
+        roleView.leftToRight(of: dateView, offset: 16)
+        roleView.topToBottom(of: fullNameLabel, offset: 21)
+        roleView.height(52)
+        roleView.width(164)
 
-        nameView.topToBottom(of: dateView, offset: 20)
-        nameView.edgesToSuperview(excluding: [.bottom, .top], insets: .left(24) + .right(24))
-        nameView.height(74)
+        fullNameView.topToBottom(of: dateView, offset: 20)
+        fullNameView.edgesToSuperview(excluding: [.bottom, .top], insets: .left(24) + .right(24))
+        fullNameView.height(74)
 
-        emailView.topToBottom(of: nameView, offset: 16)
+        emailView.topToBottom(of: fullNameView, offset: 16)
         emailView.edgesToSuperview(excluding: [.bottom, .top], insets: .left(24) + .right(24))
         emailView.height(74)
         
-        saveBtn.edgesToSuperview(excluding: [.top], insets: .left(24) + .right(24) + .bottom(99))
-        saveBtn.height(51)
+        saveButton.edgesToSuperview(excluding: [.top], insets: .left(24) + .right(24) + .bottom(99))
+        saveButton.height(51)
         
         
         activity.centerInSuperview()
@@ -237,7 +242,7 @@ extension EditProfileVC: UIImagePickerControllerDelegate & UINavigationControlle
         print(info)
         if let image = info[UIImagePickerController.InfoKey(rawValue: "UIImagePickerControllerEditedImage")] as? UIImage?  {
             guard let image = image else {return}
-            imageview.image = image
+            profileImage.image = image
             guard let data = image.jpegData(compressionQuality: 0.5) else {return}
             let dataArray = [data]
             
